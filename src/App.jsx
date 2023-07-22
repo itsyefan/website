a9371d7d500500616ad2984afbfc97a677931e75
itsyefan <ethanyipnz@gmail.com>
2023-07-23 01:41:10 +1200

    feat: finish hope page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Home/>
     </div>
   )
 }
